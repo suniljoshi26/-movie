e4d492e70c326e5ff4ab32f36fdd27ec7a8654ef
Sunil Joshi <joshisunil2601@gmail.com>
2023-02-17 19:41:25 +0530

    redux -data store

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 import { useEffect, useState } from "react";
 import { fatchDataFromapi } from "./utils/api";
-
+// import { getApiConfiguration } from "./store/homeSlice";
+import { useSelector, useDispatch } from "react-redux";
+import { getApiConfiguration } from "./store/homeSlice";
 function App() {
+  const dispatch = useDispatch();
+  const { url } = useSelector((state) => state.home);
   useEffect(() => {
     data();
   }, []);
   const data = () => {
-    fatchDataFromapi("/movie/popular").then((res) => console.log("resS", res));
+    fatchDataFromapi("/movie/popular").then(
+      (res) => (console.log("res", res), dispatch(getApiConfiguration(res)))
+    );
   };
 
-  return <div className=""></div>;
+  return <div className="">{url?.total_pages}</div>;
 }
 
 export default App;
