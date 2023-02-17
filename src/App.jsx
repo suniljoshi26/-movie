d4bd299928a77f177690c768541b8869797ae7f2
Sunil Joshi <joshisunil2601@gmail.com>
2023-02-17 13:02:20 +0530

    api testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { fatchDataFromapi } from "./utils/api";
 
 function App() {
+  useEffect(() => {
+    data();
+  }, []);
+  const data = () => {
+    fatchDataFromapi("/movie/popular").then((res) => console.log("resS", res));
+  };
+
   return <div className=""></div>;
 }
 
