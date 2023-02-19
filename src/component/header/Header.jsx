8f7def8ef77fe9bb0557f0f62da742ae6dd7f19d
Sunil Joshi <joshisunil2601@gmail.com>
2023-02-19 21:10:20 +0530

    header css

--- src/component/header/Header.jsx
+++ src/component/header/Header.jsx
@@ -1,7 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 
+import { HiOutlineSearch } from "react-icons/hi";
+import { SlMenu } from "react-icons/sl";
+import { VscChromeClose } from "react-icons/vsc";
+import { useLocation, useNavigate } from "react-router-dom";
+import ContentWrapper from "../contentWrapper/ContentWrapper";
+import "./style.scss";
+import logo from "../../assets/movix-logo.svg";
 const Header = () => {
-  return <div>Header</div>;
+  const [show, setShow] = useState("top");
+  const [lastScrooly, seiLastScrooly] = useState(0);
+  const [mobileMenu, setMobileMenu] = useState(false);
+  const [query, setQuery] = useState("");
+  const [showSearch, setShowSearch] = useState("");
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  return (
+    <header className="header">
+      <ContentWrapper>
+        <div className="logo">
+          <img src={logo} />
+        </div>
+        <ul className="menuItems">
+          <li className="menuItem">Movies</li>
+          <li className="menuItem">Tv Shows</li>
+          <li className="menuItem">
+            <HiOutlineSearch />
+          </li>
+        </ul>
+        <div className="mobileMenuItems">
+          <HiOutlineSearch />
+
+          {mobileMenu ? <VscChromeClose /> : <SlMenu />}
+        </div>
+      </ContentWrapper>
+    </header>
+  );
 };
 
 export default Header;
