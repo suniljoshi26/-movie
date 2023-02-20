5ea20f619b84f21c327e534120c84d31d459a85c
Sunil Joshi <joshisunil2601@gmail.com>
2023-02-20 18:56:37 +0530

    carouseSection

--- src/pages/home/tranding/Tranding.jsx
+++ src/pages/home/tranding/Tranding.jsx
@@ -1,7 +1,14 @@
 import React from "react";
-
+import ContentWrapper from "../../../component/contentWrapper/ContentWrapper";
+import "./style.scss";
 const Tranding = () => {
-  return <div>Tranding</div>;
+  return (
+    <div className="carouseSection">
+      <ContentWrapper>
+        <span className="carouseTitle">Tranding</span>
+      </ContentWrapper>
+    </div>
+  );
 };
 
 export default Tranding;
