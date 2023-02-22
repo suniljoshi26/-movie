36f67f8198e5da48c0e1a61f09d2b0630c1c4e5d
Sunil Joshi <joshisunil2601@gmail.com>
2023-02-22 11:42:33 +0530

    detail img

---
+++ src/pages/detail/detailBanner/DetailBanner.jsx
@@ -0,0 +1,80 @@
+import React, { useState } from "react";
+import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+import dayjs from "dayjs";
+
+import "./style.scss";
+
+import useFetch from "../../../hooks/useFetch";
+// import Genres from "../../../components/genres/Genres";
+// import CircleRating from "../../../components/circleRating/CircleRating";
+// import Img from "../../../components/lazyLoadImage/Img.jsx";
+import PosterFallback from "../../../assets/no-poster.png";
+import ContentWrapper from "../../../component/contentWrapper/ContentWrapper";
+
+const DetailBanner = ({ video, crew }) => {
+  const { mediaType, id } = useParams();
+  const { data, loading } = useFetch(`/${mediaType}/${id}`);
+  const { url } = useSelector((state) => state.home);
+  const toHoursAndMinutes = (totalMinutes) => {
+    const hours = Math.floor(totalMinutes / 60);
+    const minutes = totalMinutes % 60;
+    return `${hours}h${minutes > 0 ? ` ${minutes}m` : ""}`;
+  };
+
+  return (
+    <div className="detailsBanner">
+      {!loading ? (
+        <>
+          {!!data && (
+            <>
+              <div className="backdrop-img">
+                <img src={url.backdrop + data?.backdrop_path} />
+              </div>
+              <div className="opacity-layer"></div>
+              <ContentWrapper>
+                <div className="content">
+                  <div className="left">
+                    {data.poster_path ? (
+                      <img
+                        className="posterImg"
+                        src={url.backdrop + data.poster_path}
+                      />
+                    ) : (
+                      <img className="posterImg" src={PosterFallback} />
+                    )}
+                  </div>
+                  <div className="right">
+                    <div className="title">
+                      {`${data.name || data.title} (${dayjs(
+                        data?.release_data
+                      ).format("YYYY")})`}
+                    </div>
+                    <div className="subtitle">{data.tagline}</div>
+                  </div>
+                </div>
+              </ContentWrapper>
+            </>
+          )}
+        </>
+      ) : (
+        <div className="detailsBannerSkeleton">
+          <ContentWrapper>
+            <div className="left skeleton"></div>
+            <div className="right">
+              <div className="row skeleton"></div>
+              <div className="row skeleton"></div>
+              <div className="row skeleton"></div>
+              <div className="row skeleton"></div>
+              <div className="row skeleton"></div>
+              <div className="row skeleton"></div>
+              <div className="row skeleton"></div>
+            </div>
+          </ContentWrapper>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default DetailBanner;
