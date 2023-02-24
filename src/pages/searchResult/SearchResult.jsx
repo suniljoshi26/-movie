e9b5f1bce5d5eb923ed8c3cc0b0012b8f09bd875
Sunil Joshi <joshisunil2601@gmail.com>
2023-02-24 16:27:08 +0530

    spinner

--- src/pages/searchResult/SearchResult.jsx
+++ src/pages/searchResult/SearchResult.jsx
@@ -1,7 +1,39 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import noResults from "../../assets/no-results.png";
+import { fatchDataFromapi } from "../../utils/api";
 const SearchResult = () => {
-  return <div>SearchResult</div>;
+  const [data, setData] = useState(null);
+  const [pageNum, setPageNum] = useState(1);
+  const [loading, setLoading] = useState(false);
+  const { query } = useParams();
+  const fetchInitialData = () => {
+    setLoading(true);
+    fatchDataFromapi(`/search/multi?query=${query}&page=${pageNum}`).then(
+      (res) => {
+        setData(res);
+        setPageNum((pre) => pre + 1);
+      }
+    );
+  };
+
+  const fetchNextPageData = () => {
+    setLoading(true);
+    fatchDataFromapi(`/search/multi?query=${query}&page=${pageNum}`).then(
+      (res) => {
+        if (data?.results) {
+          setData({ ...data, results: [...data?.results, ...res.results] });
+        } else {
+          setData(res);
+        }
+        setPageNum((pre) => pre + 1);
+      }
+    );
+  };
+  useEffect(() => {
+    fetchInitialData();
+  }, [query]);
+  return <div className="searchResultsPage"></div>;
 };
 
 export default SearchResult;
